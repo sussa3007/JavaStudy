98b8b63fde5c3b4464d283e598f12d783d77f271
sussa3007 <sussa1933@gmail.com>
2022-08-26 17:18:22 +0900

    Add ClassStringStudy

---
+++ ClassString/src/ClassString.java
@@ -0,0 +1,7 @@
+public class ClassString {
+    public static void main(String[] args) {
+        char[] a = {'H','e','l','l','o'};
+        String b = new String(a);
+        System.out.println(b);
+    }
+}
